69996645b639f61271b42b8ad55bd6d735fc69e9
Juliano M Dasilva <julianomoreira@julianomoreira.com>
2016-01-26 09:06:18 -0600

    first commit

---
+++ coursera/week-two/image_share-Juju/image_share.js
@@ -0,0 +1,31 @@
+Images = new Mongo.Collection("images");
+console.log(Images.find().count());
+
+if (Meteor.isClient) {
+  console.log("I am the client");
+  var img_data = [];
+  Template.images.helpers({images:img_data});
+  Template.images.helpers({images:Images.find()});
+  var timeNow = new Date();
+  Template.myTime.helpers({timeNow});
+
+  Template.images.events({
+    'click .js-image':function(event){
+      //alert("hello");
+      console.log(event);
+      $(event.target).css("width","100px");
+    },
+    'click .js-del-image':function(event){
+      var image_id = this._id;
+      console.log(image_id);
+      $("#"+image_id).hide('slow', function(){
+        Images.remove({"_id":image_id});
+      });
+
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  console.log("I am a server");
+}
